57114ca3ac953356b4236d093b73a216a5d5c540
Shivansh <shivansh.kukreti.2004@gmail.com>
2024-02-14 21:37:50 +0530

    icons

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -16,7 +16,7 @@ export const Services = (props) => {
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className="col-md-6">
                   {" "}
-                  <i className={d.icon}></i>
+                  <i className="fa fa-fonticons">{d.icon}</i>
                   <div className="service-desc">
                     <h3>{d.name}</h3>
                     <p>{d.text}</p>
